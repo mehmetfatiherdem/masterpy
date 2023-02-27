91f5c68269fa8b26018471d3b45d8139a771a02a
mehmetfatiherdem <mehmeterdem1024@gmail.com>
2023-02-27 20:58:38 +0300

    added numbers

---
+++ General Topics/Numbers.py
@@ -0,0 +1,33 @@
+### Numbers in Python ###
+
+## integer
+x = 5
+x = -3
+
+## float
+x = 5.2
+x = -4.9
+
+### you can declare float also as scientific numbers e/E (2e3 => 2x10^3 => 2000.0)
+x = 2e3
+x = 4E5
+x = -91.5e100 # -9.15e+101
+
+## complex
+x = 5 + 5j
+x = 2j
+
+### you can print the type of a variable with type()
+print(type(x))
+
+
+### you can do type conversion with int(), float(), and complex()
+a = 2
+b = 3.8
+c = 4j # you cannot convert a complex number into another type!!
+
+float(a) # int to float => 2.0
+int(b) # float to int => 3 (rounds down)
+
+
+
