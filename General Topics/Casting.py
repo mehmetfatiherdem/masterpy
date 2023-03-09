526a81ff97fef30f1e6f718927f11705d3090f0e
mehmetfatiherdem <mehmeterdem1024@gmail.com>
2023-03-09 12:49:39 +0300

    casting added

---
+++ General Topics/Casting.py
@@ -0,0 +1,15 @@
+### Type Casting in Python ###
+
+## In Python, you have the option to specify the type of a variable using casting. This involves using constructor functions since Python is an object-oriented language that defines data types through classes, including its primitive types. 
+
+x = int(4.2) # x will be 4
+y = int("5") # y will be 5
+
+i = float(1) # i will be 1
+j = float("3") # j will be 3.0
+k = float("4.2") # k will be 4.2
+
+l = str(2) # l will be '2'
+m = str(3.0) # m will be '3.0'
+
+
