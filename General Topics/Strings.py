37d210de8102a87fb1455cac952d6f8c95ea0ada
mehmetfatiherdem <mehmeterdem1024@gmail.com>
2023-03-11 14:32:29 +0300

    add str slicing

--- General Topics/Strings.py
+++ General Topics/Strings.py
@@ -4,13 +4,28 @@
 
 txt = "Time is money"
 
-print("money" in txt) #prints True
+# print("money" in txt) #prints True
 
-if "money" in txt:
-    print("Yes, 'money' is present")
+#if "money" in txt:
+    #print("Yes, 'money' is present")
 
-print("cheap" not in txt) # prints True
+#print("cheap" not in txt) # prints True
 
-if "cheap" not in txt:
-    print("No, 'cheap' is NOT present")
-    
+#if "cheap" not in txt:
+    #print("No, 'cheap' is NOT present")
+
+## String Slicing
+
+## start including:stop including:step
+## starts from 0 by default
+
+a = "Hello, World!"
+print(a[3:8]) # lo, W
+
+print(a[:5]) # Hello
+
+print(a[7:]) # World!
+
+print(a[-5:-2]) # orl
+
+print(a[1:9:2]) # el,W
