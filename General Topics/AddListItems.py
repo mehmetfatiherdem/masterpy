04ac30c93981ec9409e8f45bd29a52b89ea574d0
mehmetfatiherdem <mehmeterdem1024@gmail.com>
2023-03-17 17:15:48 +0300

    add list items

---
+++ General Topics/AddListItems.py
@@ -0,0 +1,14 @@
+### Add List Items ###
+
+l1 = ["dog", "cat", "horse"]
+l2 = ["eagle", "dove", "crow"]
+print(l1)
+
+l1.append("turtle")
+print(l1)
+
+l1.insert(2, "duck")
+print(l1)
+
+l1.extend(l2)
+print(l1)
