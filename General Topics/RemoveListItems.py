687a03df27303be8ba5f506df05e17ae38cded68
mehmetfatiherdem <mehmeterdem1024@gmail.com>
2023-03-20 13:06:26 +0300

    remove from list methods

---
+++ General Topics/RemoveListItems.py
@@ -0,0 +1,20 @@
+### Remove List Items ###
+
+l = ["dog", "cat", "hippo"]
+l.remove("cat")
+print(l)
+
+l = ["dog", "cat", "hippo"]
+l.pop()
+l.pop(0)
+print(l)
+
+l = ["dog", "cat", "hippo"]
+del l[0]
+print(l)
+
+l = ["dog", "cat", "hippo"]
+l.clear()
+print(l)
+
+
